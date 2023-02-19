2cbe7d1907874672c4adae5f42c4294cc2f224c1
devmoaid1 <moaidmohamed1312@gmail.com>
2023-02-19 19:10:43 +0200

    added app colors

---
+++ lib/core/theme/light/app_colors_light.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColorsLight {
+  static const kScaffoldBackGroundColor = Color(0xffF5F5F5);
+  static const kAppPrimaryColor = Color(0xff78C800);
+  static const kConfidenceTextColor = Color(0xff006838);
+  static const kPollutionTextColor = Color(0xffB7823A);
+}
