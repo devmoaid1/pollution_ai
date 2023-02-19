8589a3f72b6b9bb729ec0b6f92d9e3c5435217e3
devmoaid1 <moaidmohamed1312@gmail.com>
2023-02-19 17:26:18 +0200

    added app  assets class

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,9 @@
+class AppAssets {
+  static const appIconSvg = 'assets/icons/app_icon.svg';
+  static const cameraIcon = 'assets/icons/camera.svg';
+  static const confirmIcon = 'assets/icons/confirm.svg';
+  static const galleryIcon = 'assets/icons/gallery.svg';
+  static const resubmitIcon = 'assets/icons/resubmit.svg';
+  static const retakeIcon = 'assets/icons/retake.svg';
+  static const homeGraphics = 'assets/images/home_graphics.svg';
+}
