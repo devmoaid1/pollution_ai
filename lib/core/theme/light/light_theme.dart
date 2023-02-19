ea0a69b90614d364b20dd1d95f8ef8bc67c943fe
devmoaid1 <moaidmohamed1312@gmail.com>
2023-02-19 19:29:04 +0200

    added button theme

---
+++ lib/core/theme/light/light_theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pollution_ai/core/theme/light/app_colors_light.dart';
+
+final appBarTheme = AppBarTheme(
+    elevation: 1,
+    centerTitle: true,
+    backgroundColor: Colors.white,
+    toolbarHeight: 60.h,
+    iconTheme: IconThemeData(
+      color: Colors.black,
+      size: 24.h,
+    ));
+
+final elevatedButtonTheme = ElevatedButtonThemeData(
+    style: ButtonStyle(
+        elevation: MaterialStateProperty.all(2),
+        fixedSize: MaterialStateProperty.all(Size(206.w, 52.h)),
+        padding: MaterialStateProperty.all(
+            EdgeInsets.symmetric(vertical: 13.h, horizontal: 10.w)),
+        backgroundColor:
+            MaterialStateProperty.all(AppColorsLight.kAppPrimaryColor),
+        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.r)))));
+
+final ThemeData lightTheme = ThemeData.light().copyWith(
+    scaffoldBackgroundColor: AppColorsLight.kScaffoldBackGroundColor,
+    elevatedButtonTheme: elevatedButtonTheme,
+    appBarTheme: appBarTheme);
